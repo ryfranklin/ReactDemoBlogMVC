0de7fde5514fbfb076b0afd97da9ea01f38223ed
Ryan Franklin <RFranklin@salibaspharmacy.com>
2021-04-08 12:06:21 -0700

    Add project files.

---
+++ ReactDemoBlogMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReactDemoBlogMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
